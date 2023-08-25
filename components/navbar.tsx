9750b3011af6b96a5e51b3d58c12dcbf41d4845d
Aaron Murillo <amurillo11@cnm.edu>
2023-08-25 16:48:44 -0600

    Create Sidebar For Both Regular And Mobile Screen

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,6 +8,7 @@ import { Button } from "@/components/ui/button";
 
 import { cn } from "@/lib/utils";
 import { ModeToggle } from "./mode-toggle";
+import { MobileSidebar } from "@/components/mobile-sidebar";
 
 
 const font = Poppins ({
@@ -17,9 +18,9 @@ const font = Poppins ({
 
 export const Navbar = () => {
     return (
-        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary">
+        <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-secondary h-16">
             <div className="flex items-center">
-                <Menu className="md:hidden" />
+                <MobileSidebar />
                 <Link href="/">
                     <h1 className={cn(
                         "hidden md:block text-xl md:text-3xl font-bold text-primary",
