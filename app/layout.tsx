fe4a80f7869e6158c8c24ec56600798d85a3ec07
Aaron Murillo <amurillo11@cnm.edu>
2023-08-25 15:32:44 -0600

    "Created Navbar With Added Feature"

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +19,12 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>{children}</body>
+      <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+           {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
