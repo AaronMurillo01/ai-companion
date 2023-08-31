7bfa136a1807f7222b88d275daeabc99b11cf28e
Aaron Murillo <amurillo11@cnm.edu>
2023-08-31 13:44:18 -0600

    Added Category Filter For AI

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -3,23 +3,23 @@ const { PrismaClient } = require('@prisma/client');
 const db = new PrismaClient();
 
 async function main() {
-    try { 
-        await db.category.createMany({
-            data: [
-                { name: "Famous People" },
-                { name: "Movies & TV" },
-                { name: "Musicians" },
-                { name: "Games" },
-                { name: "Animals" },
-                { name: "Philosophy" },
-                { name: "Scientists" },
-            ]
-        })
-    } catch (error){
-        console.error("Error seeding default categories", error)
-    } finally {
-        await db.$disconnect();
-    }
-};
+  try {
+    await db.category.createMany({
+      data: [
+        { name: 'Famous People' },
+        { name: 'Movies & TV' },
+        { name: 'Musicians' },
+        { name: 'Games' },
+        { name: 'Animals' },
+        { name: 'Philosophy' },
+        { name: 'Scientists' },
+      ],
+    });
+  } catch (error) {
+    console.error('Error seeding default categories:', error);
+  } finally {
+    await db.$disconnect();
+  }
+}
 
 main();
