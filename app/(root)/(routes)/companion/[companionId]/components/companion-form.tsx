d66863d784b06bd98bf3c23b748e435b26cb26a4
Aaron Murillo <amurillo11@cnm.edu>
2023-09-01 00:27:17 -0600

    Setting Up Form In Create Section

--- app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
+++ app/(root)/(routes)/companion/[companionId]/components/companion-form.tsx
@@ -1,13 +1,89 @@
 "use client";
 
+import * as z from "zod";
+import { Category, Companion } from "@prisma/client";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem, FormMessage } from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+
 interface CompanionFormProps {
-    
+    initialData: Companion | null;
+    categories: Category[];
 }
 
-export const CompanionForm = () => {
+const formSchema = z.object({
+    name: z.string().min(1, {
+        message: "Name is required.",
+    }),
+    description: z.string().min(1, {
+        message: "Description is required.",
+    }),
+    instructions: z.string().min(200, {
+        message: "Instruction require at least 200 characters.",
+    }),
+    seed: z.string().min(200, {
+        message: "Seed require at least 200 characters.",
+    }),
+    src: z.string().min(1, {
+        message: "Image is required.",
+    }),
+    categoryId: z.string().min(1, {
+        message: "Category is required.",
+    }),
+    
+})
+
+export const CompanionForm = ({
+    categories,
+    initialData
+}: CompanionFormProps) => {
+const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: initialData || {
+        name: "",
+        description: "",
+        instructions: "",
+        seed: "",
+        src: "",
+        categoryId: undefined,
+    }
+});
+
+const isLoading = form.formState.isSubmitting;
+
+const onSubmit = async (value: z.infer<typeof formSchema>) => {
+    console.log(value);
+}
+
     return (
-        <div>
-            Companion Form
+        <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
+            <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 pb-10">
+                <div className="space-y-2 w-full">
+                    <div>
+                        <h3 className="text-lg font-medium">
+                            General Information
+                        </h3>
+                        <p className="text-sm text-muted-foreground">
+                           General information about your companion. 
+                        </p>
+                    </div>
+                    <Separator className="bg-primary/10" />
+                </div>
+                < FormField 
+                name="src"
+                render={({ field }) => (
+                    <FormItem className="flex flex-col items-center justify-center space-y-4">
+                            <FormControl>
+                                Image Upload Component
+                            </FormControl>
+                            <FormMessage />
+                    </FormItem>
+                )}
+                />
+            </form>
+            </Form>
         </div>
     )
 }
